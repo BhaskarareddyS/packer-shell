03eef24dcb590ed0631b29b7952ea3245cc66a64
root <root@ip-172-31-22-197.eu-west-2.compute.internal>
2020-12-05 18:59:34 +0000

    AMI build with Packer using shell

---
+++ packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo yum install -y java-1.8.0-openjdk
+sudo yum install -y nginx
+sudo systemctl enable nginx
+
---
+++ terraform-demo/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    region =   "eu-west-2"
+    access_key =""
+    secret_key = ""
+}
+
